ccedf893d6dc24c21974574d67293f9046ff5e4b
phamsangvd <105622184+phamsangvd@users.noreply.github.com>
2023-03-13 23:25:42 +0700

    get Bill list by Dao

--- Final_Project/app/src/main/java/com/example/final_project/fragment/BillFragment.java
+++ Final_Project/app/src/main/java/com/example/final_project/fragment/BillFragment.java
@@ -15,7 +15,6 @@ import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -32,6 +31,7 @@ import com.example.final_project.adapter.BillAdapter;
 import com.example.final_project.adapter.CustomerSpinnerAdapter;
 import com.example.final_project.adapter.RoomSpinnerAdapter;
 import com.example.final_project.adapter.StatusSpinnerAdapter;
+import com.example.final_project.dao.BillDAO;
 import com.example.final_project.entity.Bill;
 import com.example.final_project.entity.Customer;
 import com.example.final_project.entity.Room;
@@ -42,9 +42,12 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SuppressLint("SimpleDateFormat")
 public class BillFragment extends Fragment {
+
+    private BillDAO billDao;
 
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
     private StatusSpinnerAdapter statusAdapter;
@@ -54,7 +57,6 @@ public class BillFragment extends Fragment {
     private RoomSpinnerAdapter roomAdapter;
 
     private int year, month, day;
-
 
     ImageView startDateBtn;
     TextView startDateTv;
@@ -69,6 +71,20 @@ public class BillFragment extends Fragment {
     Button findRoom;
     ImageView refresh;
 
+    private int id;
+
+    private int managerId;
+
+    private int customerId;
+
+    private int roomId;
+
+    private String billStartDate;
+
+    private String billEndDate;
+
+    private int billTotal;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -77,25 +93,49 @@ public class BillFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        billDao = new BillDAO(view.getContext());
         super.onViewCreated(view, savedInstanceState);
 
         RecyclerView recyclerView = view.findViewById(R.id.rcv_bill);
-        BillAdapter billAdapter = new BillAdapter(getBillList(),getContext());
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(billAdapter);
+        List<Bill> billList = getBillList(billDao);
+
 
         Spinner spnRoomStatus = view.findViewById(R.id.spinner_status_hd);
         statusAdapter = new StatusSpinnerAdapter(view.getContext(), R.layout.item_status_spinner_selected, getListStatus());
         spnRoomStatus.setAdapter(statusAdapter);
 
-
-
         spnRoomStatus.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                //todo: filter theo từng status ở đây
-                Toast.makeText(view.getContext(), statusAdapter.getItem(position).statusName, Toast.LENGTH_SHORT).show();
+                int statusId = statusAdapter.getItem(position).id;
+                switch (statusId) {
+                    case 1:
+                        //not check in
+                        List<Bill> billNotCheckIns = billList.stream()
+                                .filter(bill -> bill.getStatus() == 1)
+                                .collect(Collectors.toList());
+                        recyclerView.setLayoutManager(linearLayoutManager);
+                        recyclerView.setAdapter(new BillAdapter(billNotCheckIns, getContext()));
+                        break;
+                    case 2:
+                        List<Bill> billCheckIns = billList.stream()
+                                .filter(bill -> bill.getStatus() == 2)
+                                .collect(Collectors.toList());
+                        recyclerView.setLayoutManager(linearLayoutManager);
+                        recyclerView.setAdapter(new BillAdapter(billCheckIns, getContext()));
+                        break;
+                    case 3:
+                    default:
+                        List<Bill> billCheckOuts = billList.stream()
+                                .filter(bill -> bill.getStatus() == 3)
+                                .collect(Collectors.toList());
+                        recyclerView.setLayoutManager(linearLayoutManager);
+                        recyclerView.setAdapter(new BillAdapter(billCheckOuts, getContext()));
+                        break;
+                }
+
+
             }
 
             @Override
@@ -112,6 +152,7 @@ public class BillFragment extends Fragment {
     }
 
     private void openAddBillDialog(View view) {
+        billDao = new BillDAO(view.getContext());
         Dialog dialog = new Dialog(view.getContext());
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.bill_dialog);
@@ -127,8 +168,8 @@ public class BillFragment extends Fragment {
         winLayoutParams.gravity = Gravity.CENTER;
         window.setAttributes(winLayoutParams);
 
-        Button addBill = dialog.findViewById(R.id.add_bill);
-        Button cancel = dialog.findViewById(R.id.cancel);
+        Button addBillBtn = dialog.findViewById(R.id.add_bill);
+        Button cancelBtn = dialog.findViewById(R.id.cancel);
 
         startDateBtn = dialog.findViewById(R.id.start_date_btn);
         startDateTv = dialog.findViewById(R.id.start_date_tv);
@@ -136,13 +177,12 @@ public class BillFragment extends Fragment {
         endDateTv = dialog.findViewById(R.id.end_date_tv);
         spnCustomer = dialog.findViewById(R.id.spinner_customer);
         spnRoom = dialog.findViewById(R.id.spinner_room);
-        spnService = dialog.findViewById(R.id.spinner_service);
         cbCheckout = dialog.findViewById(R.id.checkbox_checkout);
         tvNote = dialog.findViewById(R.id.bill_note);
         totalBill = dialog.findViewById(R.id.total_bill);
 
+        View roomLayout = dialog.findViewById(R.id.linear_room);
         findRoom = dialog.findViewById(R.id.find_room_btn);
-        refresh = dialog.findViewById(R.id.refresh_btn);
 
         //Date execute
         DatePickerDialog.OnDateSetListener fromDate = (datePicker, year, month, dayOfMonth) -> {
@@ -182,29 +222,118 @@ public class BillFragment extends Fragment {
         customerAdapter = new CustomerSpinnerAdapter(view.getContext(), R.layout.item_customer_spinner, getListCustomer());
         spnCustomer.setAdapter(customerAdapter);
 
-        //List room spinner execute
-        //todo: tìm phòng theo ngày, sau khi find theo ngày thì hiện ra spinner phòng
-        roomAdapter = new RoomSpinnerAdapter(view.getContext(), R.layout.item_room_spinner, getRooms());
-        spnRoom.setAdapter(roomAdapter);
+        findRoom.setOnClickListener(button -> {
+            if (startDateTv.getText().toString().equals("") || endDateTv.getText().toString().equals("")) {
+                Toast.makeText(view.getContext(), "You must pick a date", Toast.LENGTH_SHORT).show();
+            } else {
+                GregorianCalendar start = convertStringToDate(startDateTv.getText().toString());
+                GregorianCalendar end = convertStringToDate(endDateTv.getText().toString());
+
+                if (start.compareTo(end) > 0) {
+                    Toast.makeText(view.getContext(), "Start date must be small than End date ", Toast.LENGTH_SHORT).show();
+                } else {
+                    roomLayout.setVisibility(View.VISIBLE);
+                    List<Room> getRoomsByDate = filterRoomByDate(billDao, start, end);
+                    roomAdapter = new RoomSpinnerAdapter(view.getContext(), R.layout.item_room_spinner, getRoomsByDate);
+                    spnRoom.setAdapter(roomAdapter);
+                    billStartDate = dateFormat.format(start.getTime());
+                    billEndDate = dateFormat.format(end.getTime());
+                }
+            }
+        });
+
+        spnCustomer.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                customerId = getListCustomer().get(position).getId();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        spnRoom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                roomId = getRooms().get(position).getId();
+                totalBill.setText(getRooms().get(position).getPrice() + "");
+                billTotal = Integer.parseInt(totalBill.getText().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        addBillBtn.setOnClickListener(button -> {
+            Bill bill = new Bill();
+            bill.setCustomerId(customerId);
+            bill.setRoomId(roomId);
+            bill.setFromDate(billStartDate);
+            bill.setEndDate(billEndDate);
+            bill.setStatus(1);
+            bill.setNote(tvNote.getText().toString());
+            bill.setBillDate(getNowDate());
+            bill.setBillTotal(billTotal);
+            bill.setRoomPrice(0);
+            bill.setServicePrice(0);
+            if (billDao.insert(bill) > 0) {
+                Toast.makeText(view.getContext(), "Add bill success", Toast.LENGTH_SHORT).show();
+                dialog.cancel();
+            } else {
+                Toast.makeText(view.getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         dialog.show();
-
     }
 
-    private List<Bill> getBillList() {
-        List<Bill> billList = new ArrayList<>();
-        billList.add(new Bill(1, 1, 1, 1, "12/3/2023", "13/3/2023", 1, "", "12/3/2023", 1000, 1000, 1000));
-        billList.add(new Bill(2, 1, 3, 14, "12/3/2023", "13/3/2023", 0, "", "12/3/2023", 1000, 1000, 1000));
-        billList.add(new Bill(3, 1, 2, 15, "12/3/2023", "15/3/2023", 2, "", "12/3/2023", 1000, 1000, 1000));
-        return billList;
+    private String getNowDate() {
+        Calendar date = Calendar.getInstance();
+        GregorianCalendar c = new GregorianCalendar(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH));
+        return dateFormat.format(c.getTime());
+    }
+
+    private GregorianCalendar convertStringToDate(String dateFormat) {
+        String[] s = dateFormat.split("/");
+        int year = Integer.parseInt(s[2]);
+        int month = Integer.parseInt(s[1]);
+        int day = Integer.parseInt(s[0]);
+        return new GregorianCalendar(year, month, day);
+    }
+
+    private List<Room> filterRoomByDate(BillDAO billDAO, GregorianCalendar start, GregorianCalendar end) {
+        List<Bill> filterBills = getBillList(billDAO).stream().filter(bill -> {
+            GregorianCalendar billStart = convertStringToDate(bill.getFromDate());
+            GregorianCalendar billEnd = convertStringToDate(bill.getEndDate());
+            return billStart.compareTo(start) <= 0 && billEnd.compareTo(start) <= 0 || billStart.compareTo(end) <= 0 && billEnd.compareTo(end) <= 0;
+        }).collect(Collectors.toList());
+
+        List<Integer> filterRoomIds = filterBills.stream()
+                .map(Bill::getRoomId)
+                .collect(Collectors.toList());
+
+        return getRooms().stream()
+                .filter(room -> !filterRoomIds.contains(room.getId()))
+                .collect(Collectors.toList());
+    }
+
+    private List<Bill> getBillList(BillDAO billDAO) {
+//        List<Bill> billList = new ArrayList<>();
+//        billList.add(new Bill(1, 1, 1, 1, "12/3/2023", "13/3/2023", 1, "", "12/3/2023", 1000, 1000, 1000));
+//        billList.add(new Bill(2, 1, 3, 14, "12/3/2023", "13/3/2023", 0, "", "12/3/2023", 1000, 1000, 1000));
+//        billList.add(new Bill(3, 1, 2, 15, "12/3/2023", "15/3/2023", 2, "", "12/3/2023", 1000, 1000, 1000));
+//        return billList;
+        return billDAO.getAll();
     }
 
     private List<RoomStatus> getListStatus() {
         //todo: Trả về list các status có trong db (tạm thời fix cứng data)
         List<RoomStatus> roomStatuses = new ArrayList<>();
-        roomStatuses.add(new RoomStatus(1, "Chua tra"));
-        roomStatuses.add(new RoomStatus(2, "Da tra"));
-        roomStatuses.add(new RoomStatus(3, "Chua nhan phong"));
+        roomStatuses.add(new RoomStatus(1, "Chua nhan phong"));
+        roomStatuses.add(new RoomStatus(2, "Chua tra"));
+        roomStatuses.add(new RoomStatus(3, "Da tra"));
         return roomStatuses;
     }
 
