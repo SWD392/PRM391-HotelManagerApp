0040f92e304cc9cf7b39d249fb75a61d9742f7ef
phamsangvd <105622184+phamsangvd@users.noreply.github.com>
2023-03-14 16:22:50 +0700

    fix recyclerView

--- Final_Project/app/src/main/java/com/example/final_project/adapter/BillAdapter.java
+++ Final_Project/app/src/main/java/com/example/final_project/adapter/BillAdapter.java
@@ -13,6 +13,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -23,6 +24,7 @@ import com.example.final_project.entity.Bill;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BillAdapter extends RecyclerView.Adapter<BillAdapter.BillViewHolder> {
 
@@ -50,7 +52,7 @@ public class BillAdapter extends RecyclerView.Adapter<BillAdapter.BillViewHolder
     @Override
     public void onBindViewHolder(@NonNull BillViewHolder holder, int position) {
         Bill bill = billList.get(position);
-
+        BillDAO billDAO = new BillDAO(context);
         holder.roomName.setText(bill.getRoomId() + "");
         holder.customerName.setText(bill.getCustomerId() + "");
         holder.fromDate.setText(bill.getFromDate());
@@ -68,6 +70,7 @@ public class BillAdapter extends RecyclerView.Adapter<BillAdapter.BillViewHolder
                 holder.billStatus.setText("CHECK IN");
                 holder.billStatus.setTextColor(Color.RED);
                 holder.checkInBtn.setText("Check out");
+                holder.cancelBillButton.setVisibility(View.GONE);
                 break;
             case 3:
             default:
@@ -75,7 +78,139 @@ public class BillAdapter extends RecyclerView.Adapter<BillAdapter.BillViewHolder
                 holder.billStatus.setText("CHECK OUT");
                 holder.billStatus.setTextColor(Color.GREEN);
                 holder.checkInBtn.setVisibility(View.GONE);
+                holder.cancelBillButton.setVisibility(View.GONE);
+
         }
+
+        holder.viewDetail.setOnClickListener(button -> {
+            openDialogDetail(context, bill);
+        });
+
+        holder.cancelBillButton.setOnClickListener(button -> {
+            Dialog dialog = new Dialog(context);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(R.layout.confirm_bill_dialog);
+
+            Button confirmBtn = dialog.findViewById(R.id.confirm_btn);
+            Button cancel = dialog.findViewById(R.id.cancel_btn);
+
+            confirmBtn.setOnClickListener(cfButton -> {
+                if (billDAO.deleteBill(bill.getId())) {
+                    Toast.makeText(context, "Bill was deleted", Toast.LENGTH_SHORT).show();
+                    billList.clear();
+                    billList = billDAO.getAll()
+                            .stream()
+                            .filter(billEntity -> billEntity.getStatus() == 1)
+                            .collect(Collectors.toList());
+                    dialog.cancel();
+                } else {
+                    Toast.makeText(context, "Cannot delete bill", Toast.LENGTH_SHORT).show();
+                }
+            });
+            cancel.setOnClickListener(cancelBtn -> {
+                dialog.cancel();
+            });
+            dialog.show();
+        });
+
+        holder.checkInBtn.setOnClickListener(button -> {
+            Dialog dialog = new Dialog(context);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(R.layout.confirm_bill_dialog);
+
+            Button confirmBtn = dialog.findViewById(R.id.confirm_btn);
+            Button cancel = dialog.findViewById(R.id.cancel_btn);
+
+            confirmBtn.setOnClickListener(cfButton -> {
+                //todo
+                if (bill.getStatus() == 1) {
+                    bill.setStatus(2);
+                    if (billDAO.update(bill)) {
+                        Toast.makeText(context, "Check in success", Toast.LENGTH_SHORT).show();
+                        billList.clear();
+                        billList = billDAO.getAll()
+                                .stream()
+                                .filter(billEntity -> billEntity.getStatus() == 1)
+                                .collect(Collectors.toList());
+                        notifyDataSetChanged();
+                        dialog.cancel();
+                    } else {
+                        Toast.makeText(context, "Cannot Check in", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    bill.setStatus(3);
+                    if (billDAO.update(bill)) {
+                        Toast.makeText(context, "Check out success", Toast.LENGTH_SHORT).show();
+                        billList.clear();
+                        billList = billDAO.getAll()
+                                .stream()
+                                .filter(billEntity -> billEntity.getStatus() == 2)
+                                .collect(Collectors.toList());
+                        notifyDataSetChanged();
+                        dialog.cancel();
+
+                    } else {
+                        Toast.makeText(context, "Cannot Check out", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+            cancel.setOnClickListener(cancelBtn -> {
+                dialog.cancel();
+            });
+            dialog.show();
+        });
+
+    }
+
+    private TextView startDateDetail;
+    private TextView endDateDetail;
+    private TextView customerDetail;
+    private TextView roomDetail;
+    private TextView statusDetail;
+    private TextView noteDetail;
+    private TextView totalBillDetail;
+    private Button doneViewBtn;
+
+    private void openDialogDetail(Context context, Bill bill) {
+        Dialog dialog = new Dialog(context);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bill_dialog_view_detail);
+
+        Window window = dialog.getWindow();
+
+        if (window == null) {
+            return;
+        }
+
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams winLayoutParams = window.getAttributes();
+        winLayoutParams.gravity = Gravity.CENTER;
+        window.setAttributes(winLayoutParams);
+
+        startDateDetail = dialog.findViewById(R.id.start_date_detail);
+        endDateDetail = dialog.findViewById(R.id.end_date_detail);
+        customerDetail = dialog.findViewById(R.id.customer_name_detail);
+        roomDetail = dialog.findViewById(R.id.room_detail);
+        noteDetail = dialog.findViewById(R.id.bill_note_detail);
+        statusDetail = dialog.findViewById(R.id.status_detail);
+        totalBillDetail = dialog.findViewById(R.id.total_bill_detail);
+        doneViewBtn = dialog.findViewById(R.id.done_btn);
+
+        startDateDetail.setText(bill.getFromDate());
+        endDateDetail.setText(bill.getEndDate());
+        customerDetail.setText(bill.getCustomerId() + "");
+        roomDetail.setText(bill.getRoomId() + "");
+        noteDetail.setText(bill.getNote());
+        //todo: Sửa thành status
+        statusDetail.setText(bill.getStatus() + "");
+        totalBillDetail.setText(bill.getBillTotal() + "");
+        doneViewBtn.setOnClickListener(button -> {
+            dialog.cancel();
+        });
+
+        dialog.show();
     }
 
     @Override
@@ -100,7 +235,6 @@ public class BillAdapter extends RecyclerView.Adapter<BillAdapter.BillViewHolder
 
         public BillViewHolder(@NonNull View view) {
             super(view);
-            BillDAO billDAO = new BillDAO(view.getContext());
             roomName = view.findViewById(R.id.room_name);
             customerName = view.findViewById(R.id.customer_name);
             fromDate = view.findViewById(R.id.from_date);
@@ -110,65 +244,8 @@ public class BillAdapter extends RecyclerView.Adapter<BillAdapter.BillViewHolder
             viewDetail = view.findViewById(R.id.view_detail_bill_btn);
             billStatus = view.findViewById(R.id.tv_bill_status);
             cancelBillButton = view.findViewById(R.id.cancel_bill_btn);
-
-            viewDetail.setOnClickListener(button -> {
-                openDialogDetail(view);
-            });
-
-            cancelBillButton.setOnClickListener(button -> {
-
-            });
         }
 
-        private TextView startDateDetail;
-        private TextView endDateDetail;
-        private TextView customerDetail;
-        private TextView roomDetail;
-        private TextView statusDetail;
-        private TextView noteDetail;
-        private TextView totalBillDetail;
-        private Button doneViewBtn;
 
-        private void openDialogDetail(View view) {
-            Dialog dialog = new Dialog(view.getContext());
-            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-            dialog.setContentView(R.layout.bill_dialog_view_detail);
-
-            Window window = dialog.getWindow();
-
-            if (window == null) {
-                return;
-            }
-
-            window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
-            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-
-            WindowManager.LayoutParams winLayoutParams = window.getAttributes();
-            winLayoutParams.gravity = Gravity.CENTER;
-            window.setAttributes(winLayoutParams);
-
-            startDateDetail = dialog.findViewById(R.id.start_date_detail);
-            endDateDetail = dialog.findViewById(R.id.end_date_detail);
-            customerDetail = dialog.findViewById(R.id.customer_name_detail);
-            roomDetail = dialog.findViewById(R.id.room_detail);
-            noteDetail = dialog.findViewById(R.id.bill_note_detail);
-            statusDetail = dialog.findViewById(R.id.status_detail);
-            totalBillDetail = dialog.findViewById(R.id.total_bill_detail);
-            doneViewBtn = dialog.findViewById(R.id.done_btn);
-
-            startDateDetail.setText(bill.getFromDate());
-            endDateDetail.setText(bill.getEndDate());
-            customerDetail.setText(customerName.getText().toString());
-            roomDetail.setText(roomName.getText().toString());
-            noteDetail.setText(bill.getNote());
-            //todo: Sửa thành status
-            statusDetail.setText(bill.getStatus() +"");
-            totalBillDetail.setText(bill.getBillTotal() + "");
-            doneViewBtn.setOnClickListener(button -> {
-                dialog.cancel();
-            });
-
-            dialog.show();
-        }
     }
 }
