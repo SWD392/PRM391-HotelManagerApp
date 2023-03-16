61a1a163b61b37ebf3ae141816fcc7c7d81df8aa
phamsangvd <105622184+phamsangvd@users.noreply.github.com>
2023-03-16 13:05:06 +0700

    change id to name on UI

--- Final_Project/app/src/main/java/com/example/final_project/dao/RoomDao.java
+++ Final_Project/app/src/main/java/com/example/final_project/dao/RoomDao.java
@@ -18,41 +18,46 @@ public class RoomDao {
     private SQLiteDatabase db;
 
 
-    public RoomDao(Context context){
-        dbHelper= new DbHelper(context);
-        db=dbHelper.getWritableDatabase();
+    public RoomDao(Context context) {
+        dbHelper = new DbHelper(context);
+        db = dbHelper.getWritableDatabase();
     }
-    public List<Room> getDaTa(String sql, String...selectionArgs){
-        List<Room> list=new ArrayList<>();
-        Cursor c=db.rawQuery(sql,selectionArgs);
+
+    public List<Room> getDaTa(String sql, String... selectionArgs) {
+        List<Room> list = new ArrayList<>();
+        Cursor c = db.rawQuery(sql, selectionArgs);
         if (c.getCount() > 0) {
             c.moveToFirst();
             while (!c.isAfterLast()) {
                 int a = c.getInt(0);
-                int d= c.getInt(1);
+                int d = c.getInt(1);
                 String b = c.getString(2);
-                int g=c.getInt(3);
+                int g = c.getInt(3);
                 int p = c.getInt(4);
-                list.add(new Room(a,b,d,g,p));
+                list.add(new Room(a, b, d, g, p));
                 c.moveToNext();
             }
             c.close();
         }
         return list;
     }
-    public  List<Room> getListRoom(){
+
+    public List<Room> getListRoom() {
         String query = "SELECT * FROM Room ";
         return getDaTa(query);
     }
-    public long insertRoom(Room room){
-        ContentValues values=new ContentValues();
-        values.put("status_id",room.getStatus());
-        values.put("name",room.getName());
-        values.put("price",room.getPrice());
-        values.put("room_category_id",room.getRoomTypeId());
 
+    public long insertRoom(Room room) {
+        ContentValues values = new ContentValues();
+        values.put("status_id", room.getStatus());
+        values.put("name", room.getName());
+        values.put("price", room.getPrice());
+        values.put("room_category_id", room.getRoomTypeId());
+        return db.insert("Room", null, values);
+    }
 
-        return db.insert("Room",null,values);
-
+    public List<Room> getRoomById(int id) {
+        String query = "SELECT * FROM Room where id = " + id;
+        return getDaTa(query);
     }
 }
--- Final_Project/app/src/main/java/com/example/final_project/fragment/BillFragment.java
+++ Final_Project/app/src/main/java/com/example/final_project/fragment/BillFragment.java
@@ -32,6 +32,7 @@ import com.example.final_project.adapter.CustomerSpinnerAdapter;
 import com.example.final_project.adapter.RoomSpinnerAdapter;
 import com.example.final_project.adapter.StatusSpinnerAdapter;
 import com.example.final_project.dao.BillDAO;
+import com.example.final_project.dao.RoomDao;
 import com.example.final_project.entity.Bill;
 import com.example.final_project.entity.Customer;
 import com.example.final_project.entity.Room;
@@ -40,6 +41,7 @@ import com.example.final_project.entity.RoomStatus;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -48,6 +50,8 @@ import java.util.stream.Collectors;
 public class BillFragment extends Fragment {
 
     private BillDAO billDao;
+
+    private RoomDao roomDao;
 
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
     private StatusSpinnerAdapter statusAdapter;
@@ -88,6 +92,7 @@ public class BillFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         billDao = new BillDAO(view.getContext());
+        roomDao = new RoomDao(view.getContext());
         super.onViewCreated(view, savedInstanceState);
 
         getBillRecyclerView(view);
@@ -102,6 +107,7 @@ public class BillFragment extends Fragment {
 
     private void openAddBillDialog(View view) {
         billDao = new BillDAO(view.getContext());
+        roomDao = new RoomDao(view.getContext());
         Dialog dialog = new Dialog(view.getContext());
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.bill_dialog);
@@ -132,6 +138,10 @@ public class BillFragment extends Fragment {
 
         View roomLayout = dialog.findViewById(R.id.linear_room);
         findRoom = dialog.findViewById(R.id.find_room_btn);
+
+        cancelBtn.setOnClickListener(button -> {
+            dialog.cancel();
+        });
 
         //Date execute
         DatePickerDialog.OnDateSetListener fromDate = (datePicker, year, month, dayOfMonth) -> {
@@ -177,12 +187,11 @@ public class BillFragment extends Fragment {
             } else {
                 GregorianCalendar start = convertStringToDate(startDateTv.getText().toString());
                 GregorianCalendar end = convertStringToDate(endDateTv.getText().toString());
-
                 if (start.compareTo(end) > 0) {
                     Toast.makeText(view.getContext(), "Start date must be small than End date ", Toast.LENGTH_SHORT).show();
-                } else {
+                } else if (start.compareTo(end) <= 0) {
                     roomLayout.setVisibility(View.VISIBLE);
-                    List<Room> getRoomsByDate = filterRoomByDate(billDao, start, end);
+                    List<Room> getRoomsByDate = filterRoomByDate(roomDao, billDao, start, end);
                     roomAdapter = new RoomSpinnerAdapter(view.getContext(), R.layout.item_room_spinner, getRoomsByDate);
                     spnRoom.setAdapter(roomAdapter);
                     billStartDate = dateFormat.format(start.getTime());
@@ -205,8 +214,8 @@ public class BillFragment extends Fragment {
         spnRoom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                roomId = getRooms().get(position).getId();
-                totalBill.setText(getRooms().get(position).getPrice() + "");
+                roomId = getRooms(roomDao).get(position).getId();
+                totalBill.setText(getRooms(roomDao).get(position).getPrice() + "");
                 billTotal = Integer.parseInt(totalBill.getText().toString());
             }
 
@@ -296,23 +305,24 @@ public class BillFragment extends Fragment {
     private GregorianCalendar convertStringToDate(String dateFormat) {
         String[] s = dateFormat.split("/");
         int year = Integer.parseInt(s[2]);
-        int month = Integer.parseInt(s[1]);
+        int month = Integer.parseInt(s[1]) - 1;
         int day = Integer.parseInt(s[0]);
         return new GregorianCalendar(year, month, day);
     }
 
-    private List<Room> filterRoomByDate(BillDAO billDAO, GregorianCalendar start, GregorianCalendar end) {
-        List<Bill> filterBills = getBillList(billDAO).stream().filter(bill -> {
-            GregorianCalendar billStart = convertStringToDate(bill.getFromDate());
-            GregorianCalendar billEnd = convertStringToDate(bill.getEndDate());
-            return billStart.compareTo(start) <= 0 && billEnd.compareTo(start) <= 0 || billStart.compareTo(end) <= 0 && billEnd.compareTo(end) <= 0;
-        }).collect(Collectors.toList());
+    private List<Room> filterRoomByDate(RoomDao roomDao, BillDAO billDAO, GregorianCalendar start, GregorianCalendar end) {
+        List<Bill> filterBills = getBillList(billDAO).stream()
+                .filter(bill -> {
+                    Date billStart = convertStringToDate(bill.getFromDate()).getTime();
+                    Date billEnd = convertStringToDate(bill.getEndDate()).getTime();
+                    return (billStart.compareTo(start.getTime()) <= 0 && billEnd.compareTo(start.getTime()) >= 0) || (billStart.compareTo(end.getTime()) <= 0 && billEnd.compareTo(end.getTime()) >= 0);
+                }).collect(Collectors.toList());
 
         List<Integer> filterRoomIds = filterBills.stream()
                 .map(Bill::getRoomId)
                 .collect(Collectors.toList());
 
-        return getRooms().stream()
+        return getRooms(roomDao).stream()
                 .filter(room -> !filterRoomIds.contains(room.getId()))
                 .collect(Collectors.toList());
     }
@@ -324,9 +334,9 @@ public class BillFragment extends Fragment {
     private List<RoomStatus> getListStatus() {
         //todo: Trả về list các status có trong db (tạm thời fix cứng data)
         List<RoomStatus> roomStatuses = new ArrayList<>();
-        roomStatuses.add(new RoomStatus(1, "Chua nhan phong"));
-        roomStatuses.add(new RoomStatus(2, "Chua tra"));
-        roomStatuses.add(new RoomStatus(3, "Da tra"));
+        roomStatuses.add(new RoomStatus(1, "Not Check In"));
+        roomStatuses.add(new RoomStatus(2, "Checked In"));
+        roomStatuses.add(new RoomStatus(3, "Checked Out"));
         return roomStatuses;
     }
 
@@ -339,12 +349,13 @@ public class BillFragment extends Fragment {
         return customers;
     }
 
-    private List<Room> getRooms() {
-        List<Room> rooms = new ArrayList<>();
-        rooms.add(new Room(1, "1", 1, 100, 1));
-        rooms.add(new Room(2, "2", 2, 100, 1));
-        rooms.add(new Room(3, "3", 3, 100, 1));
-        rooms.add(new Room(4, "4", 1, 100, 1));
-        return rooms;
+    private List<Room> getRooms(RoomDao roomDao) {
+//        List<Room> rooms = new ArrayList<>();
+//        rooms.add(new Room(1, "1", 1, 100, 1));
+//        rooms.add(new Room(2, "2", 2, 100, 1));
+//        rooms.add(new Room(3, "3", 3, 100, 1));
+//        rooms.add(new Room(4, "4", 1, 100, 1));
+//        return rooms;
+        return roomDao.getListRoom();
     }
 }
